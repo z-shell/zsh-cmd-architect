103957f9bcd5a515a78193ec11a445190c00bf0f
Salvydas Lukosius <sall@w-ss.io>
2023-01-29 17:34:48 +0000

    modified:   doc/install.sh 	modified:   docs/README.md

--- doc/install.sh
+++ doc/install.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2039,SC2154,SC3054
 
 if ! type git 2>/dev/null 1>&2; then
-	echo "Please install GIT first"
-	echo "Exiting"
-	exit 1
+  echo "Please install GIT first"
+  echo "Exiting"
+  exit 1
 fi
 
 #
@@ -12,20 +12,20 @@ fi
 #
 
 if ! test -d "$HOME/.config"; then
-	mkdir "$HOME/.config"
+  mkdir "$HOME/.config"
 fi
 
 if ! test -d "$HOME/.config/zca"; then
-	mkdir "$HOME/.config/zca"
+  mkdir "$HOME/.config/zca"
 fi
 
 echo ">>> Downloading zsh-cmd-architect to ~/.config/zca"
 if test -d ~/.config/zca/zsh-cmd-architect; then
-	cd ~/.config/zca/zsh-cmd-architect || exit
-	git pull origin main
+  cd ~/.config/zca/zsh-cmd-architect || exit
+  git pull origin main
 else
-	cd ~/.config/zca || exit
-	git clone https://github.com/z-shell/zsh-cmd-architect.git zsh-cmd-architect
+  cd ~/.config/zca || exit
+  git clone https://github.com/z-shell/zsh-cmd-architect.git zsh-cmd-architect
 fi
 echo ">>> Done"
 
@@ -40,9 +40,9 @@ cd ~/.config/zca || exit
 set h-list.conf zca.conf
 
 for i; do
-	if ! test -f "$i"; then
-		cp -v zsh-cmd-architect/.config/zca/"$i" .
-	fi
+  if ! test -f "$i"; then
+    cp -v zsh-cmd-architect/.config/zca/"$i" .
+  fi
 done
 
 echo ">>> Done"
@@ -53,10 +53,9 @@ echo ">>> Done"
 
 echo ">>> Updating .zshrc"
 if ! grep zsh-cmd-architect ~/.zshrc >/dev/null 2>&1; then
-cat << EOF > ~/.zshrc
+  cat <<'EOF' >>~/.zshrc
 
 ### ZCA's installer added snippet ###
-# trunk-ignore(shfmt/parse)
 fpath=( "${fpath[@]}" "${HOME}/.config/zca/zsh-cmd-architect" )
 autoload h-list zca zca-usetty-wrapper zca-widget
 zle -N zca-widget
@@ -66,5 +65,5 @@ bindkey '^T' zca-widget
 EOF
 
 else
-	echo ">>> .zshrc already updated, not making changes"
+  echo ">>> .zshrc already updated, not making changes"
 fi
